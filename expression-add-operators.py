1196eff39d2968e8e0588b8073f42648cf4e5619
Shawn Axsom <axs221@gmail.com>
2016-11-26 15:03:20 -0500

    expression-add-operators is working except for edge cases

--- expression-add-operators.py
+++ expression-add-operators.py
@@ -7,9 +7,10 @@ class Solution(object):
         self.operators = ["+", "-", "*"]
 
     def getAllFormulas(self, nums):
-        print "nums: %s" % (nums)
         if len(nums) == 0:
             return []
+        elif type(nums) is not list:
+            return [nums]
 
         nums_with_operators = [[p + o for o in self.operators] for p in nums[:-1]] + [[nums[-1]]]
         combinations = list(itertools.product(*nums_with_operators))
@@ -17,19 +18,42 @@ class Solution(object):
 
         return formulas
 
-    def getNumberCombinations(self, num):
-        nums = list(num)
-        return [nums]
+    def flatten(self, possibly_nested_list):
+        if len(possibly_nested_list) > 0 and type(possibly_nested_list[0]) is list:
+            return list(itertools.chain(*possibly_nested_list))
+        else:
+            return possibly_nested_list
+
+    def getNumberCombinations(self, nums):
+        allCombinations = []
+
+        for i in range(len(nums)):
+            num = ''.join(nums[:i+1])
+            other_nums = nums[i+1:]
+            combinations = self.getNumberCombinations(other_nums)
+
+            combined = []
+            if len(combinations) > 0:
+                combined = [[num] + self.flatten(combo) for combo in combinations]
+            else:
+                combined = [num]
+
+            allCombinations.append(combined)
+
+        return allCombinations
 
     def addOperators(self, num, target):
         solutions = []
-        formulas = []
+        all_formulas = []
 
-        numberCombinations = self.getNumberCombinations(num)
+        nums = list(num)
+        numberCombinations = self.flatten(self.getNumberCombinations(nums))
+
         for numberCombination in numberCombinations:
-            formulas += self.getAllFormulas(numberCombination)
+            formulas = self.getAllFormulas(numberCombination)
+            all_formulas += formulas
 
-        for formula in formulas:
+        for formula in all_formulas:
             if eval(formula) == target:
                 solutions.append(formula)
 
@@ -37,8 +61,10 @@ class Solution(object):
 
 if __name__ == '__main__':
     s = Solution()
-    print s.addOperators("123", 6)
-    # print s.addOperators("123", 15)
-    # print s.addOperators("1235", 29)
-    # print s.addOperators("", 5)
-    # s.getAllFormulas(["1", "2", "3"])
+    print "--------------------------------------------------------------------------------------------------------"
+    # print "123, 123:", s.addOperators("123", 123)
+    # print "123, 6:", s.addOperators("123", 6)
+    print "105, 5:", s.addOperators("105", 5)
+    # print "123, 15:", s.addOperators("123", 15)
+    # print "1235, 29:", s.addOperators("1235", 29)
+    # print "[], 5:", s.addOperators("", 5)
